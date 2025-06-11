06c4e6dcbe4d31f24fb65aa8037320a4f6c60dbb
Mohd Zulhilmi Mohd Zain <zct823@gmail.com>
2025-06-11 08:40:37 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SwiftQRCodeScanner",
-            targets: ["SwiftQRCodeScanner"]),
+            name: "SwiftyQR",
+            targets: ["SwiftyQR"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
